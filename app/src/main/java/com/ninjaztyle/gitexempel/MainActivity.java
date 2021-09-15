bf8e31e6becb10aca8999202151eaa521fe10bad
nz-bill <bill.palmestedt@zocom.se>
2021-09-15 13:18:08 +0200

    first commit

---
+++ app/src/main/java/com/ninjaztyle/gitexempel/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ninjaztyle.gitexempel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int i = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
